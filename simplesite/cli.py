f69109327af8fa9dc5caed1dd279addfdb61d9ca
TheNewPhoenix <99jdoll@gmail.com>
2018-07-17 02:07:01 -0400

    Open in new browser
    -serve now opens site in browswer

--- simplesite/cli.py
+++ simplesite/cli.py
@@ -17,8 +17,8 @@ class SimpleSite(object):
     generator = self.Generator(path, title)
     generator.generate()
   
-  def serve(self, path, port):
-    server = self.Server(path, port)
+  def serve(self, path, port, browser):
+    server = self.Server(path, port, browser)
     server.serve()
 
 def parse_args(site):
@@ -29,19 +29,20 @@ def parse_args(site):
 
   subparsers.required = True
 
-  parser_create = subparsers.add_parser('create', help='Creates a new project')
-  parser_create.add_argument('title', help='The title of your website')
+  parser_create = subparsers.add_parser('create', help='Creates a new project.')
+  parser_create.add_argument('title', help='The title of your website.')
   parser_create.set_defaults(target=site.create)
 
   parser_serve = subparsers.add_parser('serve', help='Serves the project')
-  parser_serve.add_argument('-p', '--port', default=8000, type=int, help='Port that the website is served from')
+  parser_serve.add_argument('-p', '--port', default=8000, type=int, help='Port that the website is served from.')
+  parser_serve.add_argument('-b', '--browser', action='store_true', help='Whether to open in browser.')
   parser_serve.set_defaults(target=site.serve)
   
   commands = [parser_create, parser_serve]
   for command in commands:
-    command.add_argument('-d', '--path', default=os.getcwd(), help='The path of the current project')
-    command.add_argument('-v', '--verbose', action='store_true', help='Be more verbose')
-    command.add_argument('-q', '--quiet', action='store_true', help='Be quieter')
+    command.add_argument('-d', '--path', default=os.getcwd(), help='The path of the current project.')
+    command.add_argument('-v', '--verbose', action='store_true', help='Be more verbose.')
+    command.add_argument('-q', '--quiet', action='store_true', help='Be quieter.')
 
   args = parser.parse_args()
 
--- simplesite/server.py
+++ simplesite/server.py
@@ -1,21 +1,29 @@
 import logging
 import BaseHTTPServer
 import SimpleHTTPServer
+import webbrowser
 
 logger = logging.getLogger(__name__)
 
 class Server(object):
 
-  def __init__(self, path, port):
+  def __init__(self, path, port, browser):
     self.path = path
     self.port = port
+    self.browser = browser
 
   def serve(self):
     try:
       server = BaseHTTPServer.HTTPServer(('', self.port), SimpleHTTPServer.SimpleHTTPRequestHandler)
+      
       logger.info("Serving at port %s.", self.port)
-
+      
+      if self.browser is True:
+        logger.info("Opening in browser")
+        webbrowser.open('http://127.0.0.1:%s' % self.port)
+      
       server.serve_forever()
+
     except KeyboardInterrupt:
       logger.info("Shutting down server.")
       server.socket.close()
